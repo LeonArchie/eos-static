daf639bd49af0ed00504e5da3c7ca6611356676c
LeonArchie <levmikhailovish@yandex.ru>
2025-12-25 16:44:22 +0300

    echo

--- www/html/dev-eos.school59-ekb.ru/platform/include/visible/footer.php
+++ www/html/dev-eos.school59-ekb.ru/platform/include/visible/footer.php
@@ -3,6 +3,6 @@
 
 <!-- Подвал -->
 <footer class="footer">
-    <div class="version">Version: <?php VERSION_WEB ?></div>
+    <div class="version">Version: <?php echo VERSION_WEB ?></div>
     <div class="version">AGPL-3.0-only WITH LICENSE-ADDITIONAL, 2025</div>
 </footer>
