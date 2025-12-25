db05915261326270464efd844a1f7e3b27511e6f
LeonArchie <levmikhailovish@yandex.ru>
2025-12-25 16:21:42 +0300

    Version update

--- www/html/dev-eos.school59-ekb.ru/platform/include/function.php
+++ www/html/dev-eos.school59-ekb.ru/platform/include/function.php
@@ -1,7 +1,11 @@
 <?php
     //SPDX-License-Identifier: AGPL-3.0-only WITH LICENSE-ADDITIONAL
     //Copyright (C) 2025 Петунин Лев Михайлович
-    
+
+    if (!defined('VERSION_WEB')) {
+        define('VERSION_WEB', '0.9.1');
+    }
+
     // Определение переменных
     if (!defined('LOGGER_PATH')) {
         define('LOGGER_PATH', '/var/log/nginx/web.log');
--- www/html/dev-eos.school59-ekb.ru/platform/include/visible/footer.php
+++ www/html/dev-eos.school59-ekb.ru/platform/include/visible/footer.php
@@ -1,29 +1,8 @@
-<?php
-    //SPDX-License-Identifier: AGPL-3.0-only WITH LICENSE-ADDITIONAL
-    //Copyright (C) 2025 Петунин Лев Михайлович
-
-    // Определяем корень веб-сервера
-    if (!defined('ROOT_PATH')) {
-        define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
-    }
-
-    logger("INFO", "Футер начал загрузку");
-
-    include ROOT_PATH . "/platform/include/api/getVersionFromApi.php";
-
-    try {
-        $currentVersion = getVersionFromApi();
-    } catch (Throwable $e) {
-        logger("ERROR", "Failed to get version: " . $e->getMessage());
-        $currentVersion = '0.0.0.0';
-    }
-
-    // Логируем успешное завершение загрузки футера
-    logger("INFO", "Футер загружен успешно. Версия: " . $currentVersion);
-?>
+<!-- SPDX-License-Identifier: AGPL-3.0-only WITH LICENSE-ADDITIONAL
+# Copyright (C) 2025 Петунин Лев Михайлович -->
 
 <!-- Подвал -->
 <footer class="footer">
-    <div class="version">Version: <?php echo htmlspecialchars($currentVersion); ?></div>
+    <div class="version">Version: <?php VERSION_WEB ?></div>
     <div class="version">AGPL-3.0-only WITH LICENSE-ADDITIONAL, 2025</div>
 </footer>
