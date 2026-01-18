47b5aef171f07ff83e6ea815b8b64fbfb2b667cf
LeonArchie <levmikhailovish@yandex.ru>
2026-01-18 22:11:52 +0300

    TEST

--- www/html/dev-eos.school59-ekb.ru/public/index.php
+++ www/html/dev-eos.school59-ekb.ru/public/index.php
@@ -2,19 +2,19 @@
     //SPDX-License-Identifier: AGPL-3.0-only WITH LICENSE-ADDITIONAL
     //Copyright (C) 2025 Петунин Лев Михайлович
 
-    // if (!defined('PUBLIC_ROOT_PATH')) {
-    //     define('PUBLIC_ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
-    // }
+    if (!defined('PUBLIC_ROOT_PATH')) {
+        define('PUBLIC_ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
+    }
 
-    // if (!defined('PRIVATE_ROOT_PATH')) {
-    //     define('PRIVATE_ROOT_PATH', (dirname($_SERVER['DOCUMENT_ROOT'])) . '/private');
-    // }
+    if (!defined('PRIVATE_ROOT_PATH')) {
+        define('PRIVATE_ROOT_PATH', (dirname($_SERVER['DOCUMENT_ROOT'])) . '/private');
+    }
 
-    // $file_path = PRIVATE_ROOT_PATH . '/init.php';
-    // if (!@require_once $file_path) {
-    //     header("Location: " . PRIVATE_ROOT_PATH . "/err/50x.html");
-    //     exit();
-    // }
+    $file_path = PRIVATE_ROOT_PATH . '/init.php';
+    if (!@require_once $file_path) {
+        header("Location: " . PRIVATE_ROOT_PATH . "/err/50x.html");
+        exit();
+    }
 
     // $file_path = PRIVATE_ROOT_PATH . '/platform/functions/check_auth.php';
     // if (!@require_once $file_path) {
